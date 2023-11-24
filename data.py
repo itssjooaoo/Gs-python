296ace9c2b3e0e3c46afb0b3cba7f1920194306f
joao <joaopp.1502@gmail.com>
2023-11-24 09:46:55 -0300

    Para continuar em casa

---
+++ data.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+datetime.now()
+data=datetime.fromisoformat("2023-11-24")
+print(data)
+
+
+
+consulta=[["joao", "2023-4-15", "febre"], ["herbert", "2023-1-13", "dor"]]
+
+def adiciona_ocorrencia():
+    usuario = input("digite nome do usuario: ")
+    data = input("digite a data da ocorrencia: ")
+    ocorrencia = input("digite a ocorrencia: ")
+    consulta.append([usuario, data, ocorrencia])
+
+
+usuario=input("digite nome do usuario")
+for registro in consulta:
+     if registro[0]== usuario:
+             print(registro)
+
+
+
+
+             usuario = input("Área destinada a consultas passadas, de o nome do usuario ")
+        data = (input("Gostaria de consultar a data? S ou n"))
+        if data == "S":
+            data_consulta=datetime.fromisoformat(input("Deseja consultar qual data de consulta:(2022-02-15, 2022-06-23) Digite a data no formato \"yyyy-mm-dd\""))
+        if data_consulta== "2022-02-15":
+            print("Nessa consulta você estava com dor de garganta, \n ninfonodos inchados com alteração de BPM, foi-lhe recomendado uma tomografia")
+        if data_consulta == "2022-06-23":
+            print("")
--- gs.py
+++ gs.py
@@ -1,1 +1,47 @@
-print("teste")
+
+from datetime import datetime
+
+consulta=[["joao", "2023-4-15", "febre"], ["herbert", "2023-1-13", "dor"]]
+def adiciona_ocorrencia():
+    usuario = input("digite nome do usuario: ")
+    data = input("digite a data da ocorrencia: ")
+    ocorrencia = input("digite a ocorrencia: ")
+    consulta.append([usuario, data, ocorrencia])
+
+def lista_ocorrencias():
+    for registro in consulta:
+        print(registro)
+def consultar_consulta(usuario, data,):
+    consulta.append((usuario, data,))
+
+while True:
+    print("\n1. Consulta")
+    print("2. Saúde")
+    print("3. Exames")
+    print("4. Agendamento")
+    print("5. Lembretes")
+
+    opcao = int(input("Fale a opção que deseja acessar: "))
+
+    if opcao == 1:
+        adiciona_ocorrencia()
+    if opcao == 2:
+        lista_ocorrencias()
+
+    
+        
+        data = (input("Gostaria de consultar a data? S ou n"))
+        if data == "S":
+           consulta=[["joao", "2023-4-15", "febre"], ["herbert", "2023-1-13", "dor"]]
+        
+
+        print(f"Nesta data {data_consulta} você teve alteração sanguinea grave")
+
+
+        quantidade = int(input("Digite a quantidade: "))
+        consultar_consulta(usuario, data, ocorrencia)
+    elif opcao == 2:
+        listar_estoque()
+    elif opcao == 3:
+        nome = input("Digite o nome do produto: ")
+        quantidade = int(input("Digite a quantidade a ser adicionada ou subtraida: "))
