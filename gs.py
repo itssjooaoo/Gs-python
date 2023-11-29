aefc3bd4362fabe11865b81138824714873ec918
Joao <joaopp.1502@gmail.com>
2023-11-29 10:23:01 -0300

    Finalizado o projeto, com todas as opções do while  true aplicadas, e listas criadas

--- gs.py
+++ gs.py
@@ -14,6 +14,19 @@ def lista_ocorrencias():
 def consultar_consulta(usuario, data,):
     consulta.append((usuario, data,))
 
+def agendar_consulta(nome_area, lista_consultas):
+    consulta = {'Nome da Área': nome_area, 'Status': 'Agendado'}
+    lista_consultas.append(consulta)
+
+def ver_consultas(lista_consultas):
+    print("\nConsultas Agendadas:")
+    for consulta in lista_consultas:
+        print(f"Área: {consulta['Nome da Área']} - Status: {consulta['Status']}")
+
+lista_consultas = []
+
+
+
 while True:
     print("\n1. Consulta")
     print("2. Saúde")
@@ -27,21 +40,16 @@ while True:
         adiciona_ocorrencia()
     if opcao == 2:
         lista_ocorrencias()
-
-    
+    if opcao == 3:
+        # Na área de exames mostramos exames pendentes onde você deve ver os exames pedidos e agendar na opção 4
+        print(" Você tem a fazer os seguintes exames: Hemograma, Glicemia em jejum e exame de urina. Vá até a opção Agendamento para agendalos.")
+    if opcao == 4:
+        nome_area = input("Digite o nome da área para agendar a consulta: ")
+        agendar_consulta(nome_area, lista_consultas)
+    if opcao == 5:
+        ver_consultas(lista_consultas)
+    if opcao == 6:
+        break
+    else:
+        ("opção invalida.")
         
-        data = (input("Gostaria de consultar a data? S ou n"))
-        if data == "S":
-           consulta=[["joao", "2023-4-15", "febre"], ["herbert", "2023-1-13", "dor"]]
-        
-
-        print(f"Nesta data {data_consulta} você teve alteração sanguinea grave")
-
-
-        quantidade = int(input("Digite a quantidade: "))
-        consultar_consulta(usuario, data, ocorrencia)
-    elif opcao == 2:
-        listar_estoque()
-    elif opcao == 3:
-        nome = input("Digite o nome do produto: ")
-        quantidade = int(input("Digite a quantidade a ser adicionada ou subtraida: "))
